72796dc0c348b3021ed8028dc1416f410ba19482
Daniel Csonka <daniel.csonka987@gmail.com>
2019-10-21 09:57:51 +0200

    View of builderPat ready

--- OOP_ProgPat_Builder/src/application/MainWindowController.java
+++ OOP_ProgPat_Builder/src/application/MainWindowController.java
@@ -1,5 +1,47 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainWindowController {
-	
+	 @FXML
+	    private TableView<?> tableEmployee;
+
+	    @FXML
+	    private TextField txtFieldName1;
+
+	    @FXML
+	    private Button btnAddOne;
+
+	    @FXML
+	    private Button btnAddTwo;
+
+	    @FXML
+	    private ComboBox<?> cmbbxPosit;
+
+	    @FXML
+	    private ComboBox<?> cmbbxArea;
+
+	    @FXML
+	    private TextField txtFieldName2;
+
+	    @FXML
+	    private ComboBox<?> cmbbxGender;
+
+	    @FXML
+	    private TextField txtFieldBirth;
+
+	    @FXML
+	    void saveFirstFormContent(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void saveSecondFromContent(ActionEvent event) {
+
+	    }
 }

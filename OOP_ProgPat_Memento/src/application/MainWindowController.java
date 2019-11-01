03b6ced41c8be9756c52a4a89e3ea2b534cf056a
Daniel Csonka <daniel.csonka987@gmail.com>
2019-11-01 19:35:15 +0100

    project finished, func. enchanced

--- OOP_ProgPat_Memento/src/application/MainWindowController.java
+++ OOP_ProgPat_Memento/src/application/MainWindowController.java
@@ -3,6 +3,8 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.text.Caret;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,25 +25,67 @@ public class MainWindowController implements Initializable {
     @FXML
     private Button btnRedo;
 
-    @FXML
-    void redoText(ActionEvent event) {
+    private TextCaretaker cont;
+    private TextOriginator orig;
+    private Integer textIndexer;
+    private Integer savedFiles;
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		txtAreaText.setWrapText(true);
+		cont = new TextCaretaker();
+		orig = new TextOriginator();
+		orig.setSavedText("");
+		cont.addNewMemento(orig.storeNewTextMemento());
+		textIndexer = 0;
+		savedFiles = 1;
+	}
+    
 
-    }
 
     @FXML
     void saveText(ActionEvent event) {
-
+    	
+    	if(txtAreaText.getText().equals(""))
+    		return;
+    	orig.setSavedText(txtAreaText.getText());
+    	cont.addNewMemento(orig.storeNewTextMemento());
+    	textIndexer++;
+    	savedFiles++;
     }
 
     @FXML
     void undoText(ActionEvent event) {
-
+    	
+    	if(textIndexer >= 1){
+    		textIndexer--;
+			txtAreaText.setText(orig.restoreTextMemento(cont.getMemento(textIndexer)));
+		}
     }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		txtAreaText.setWrapText(true);
-		
-	}
+    @FXML
+    void redoText(ActionEvent event) {
 
+    	if(textIndexer < savedFiles){
+    		textIndexer++;
+    		if(textIndexer == savedFiles)
+    			textIndexer--;
+    		txtAreaText.setText(
+    				orig.restoreTextMemento(cont.getMemento(textIndexer))
+    				);
+    	}
+    }
+    
+    @FXML
+    void removeText(ActionEvent event){
+    	
+    	if(textIndexer < savedFiles && textIndexer >= 1){
+    		if(cont.remveMementi(orig.removeTextMementi(textIndexer))){
+    			savedFiles--;
+    		}
+    	}
+    	
+    }
+    
 }

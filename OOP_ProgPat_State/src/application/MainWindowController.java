946390cc00fc8f087c6c99d936e4ec9fa14349ad
Daniel Csonka <daniel.csonka987@gmail.com>
2019-11-02 12:48:50 +0100

    View of statusPat ready

--- OOP_ProgPat_State/src/application/MainWindowController.java
+++ OOP_ProgPat_State/src/application/MainWindowController.java
@@ -1,5 +1,46 @@
 package application;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MainWindowController implements Initializable {
 	
+	@FXML
+	private Button btnPay;
+
+	@FXML
+	private Button btnEnter;
+
+	@FXML
+	private TextField txtBxFee;
+
+	@FXML
+	private Label lblResult;
+
+	@FXML
+	private Label lblPaymentStatus;
+
+	@FXML
+	void enterTheGate(ActionEvent event) {
+
+	}
+
+	@FXML
+	void payTheFee(ActionEvent event) {
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
